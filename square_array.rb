88cf4ab44b71c71578bc411ee976b359bcb7392f
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-14 05:43:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
-  # your code here
+
+
+
+
+def square_array(numbers)
+  numbers = [1,2,3]
+  numbers.each do |numbers|
+    numbers = numbers**2
+    new_numbers = [numbers]
+    numbers<<new_numbers
+  end
 end
